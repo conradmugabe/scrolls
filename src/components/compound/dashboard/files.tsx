3804f1cd625ca12e1069d909f1b37c5bbf5ceec0
conrad mugabe <conradmugabe@gmail.com>
2023-10-05 20:31:37 +0300

    feat: added files view component

--- src/components/compound/dashboard/files.tsx
+++ src/components/compound/dashboard/files.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
 import { $Enums } from "@prisma/client";
+import { format } from "date-fns";
+import { MessageSquare, Plus, Trash } from "lucide-react";
 
 import { NoFiles } from "./no-files";
+import { Button } from "@/components/common/button";
 
 type IFile = {
   createdAt: string;
@@ -31,7 +36,35 @@ export function Files({ files }: Props) {
           <li
             key={file.id}
             className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg"
-          ></li>
+          >
+            <Link
+              href={`/dashboard/${file.id}`}
+              className="flex flex-col gap-2"
+            >
+              <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
+                <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                <div className="flex-1 truncate">
+                  <div className="flex items-center space-x-3">
+                    <h3 className="truncate text-lg font-medium text-zinc-900">
+                      {file.name}
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </Link>
+            <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+              <div className="flex items-center gap-2">
+                <Plus className="h-4 w-4" />
+                {format(new Date(file.createdAt), "MMM yyyy")}
+              </div>
+              <div>
+                <MessageSquare className="h-4 w-4" />
+              </div>
+              <Button size="sm" className="w-full" variant="destructive">
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+          </li>
         ))}
     </ul>
   );
