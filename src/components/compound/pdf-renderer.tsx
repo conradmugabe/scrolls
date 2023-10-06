d078f76e84a4b49f4e90018d7386a566f60718ab
conrad mugabe <conradmugabe@gmail.com>
2023-10-06 14:13:42 +0300

    feat: added zoom in & out functionality on the pdf component

--- src/components/compound/pdf-renderer.tsx
+++ src/components/compound/pdf-renderer.tsx
@@ -5,11 +5,12 @@ import { useState } from "react";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
+import SimpleBar from "simplebar-react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useResizeDetector } from "react-resize-detector";
-import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2, Search } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -17,6 +18,12 @@ import { useToast } from "@/components/common/toast/use-toast";
 import { Button } from "@/components/common/button";
 import { Input } from "@/components/common/input";
 import { cn } from "@/utils/cn-utils";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "@/components/common/dropdown-menu";
 
 type Props = {
   url: string;
@@ -34,6 +41,7 @@ export function PDFRenderer({ url }: Props) {
   const { toast } = useToast();
   const [numberOfPages, setNumberOfPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [scale, setScale] = useState(1);
   const { width, ref } = useResizeDetector();
 
   const PageValidator = z.object({
@@ -112,32 +120,58 @@ export function PDFRenderer({ url }: Props) {
             <ChevronUp className="h-4 w-4" />
           </Button>
         </div>
-        <div className="space-x-2"></div>
+        <div className="space-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="gap-1.5" aria-label="zoom" variant="ghost">
+                <Search className="h-4 w-4" />
+                {scale * 100}% <ChevronDown className="h-3 w-3 opacity-50" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onSelect={() => setScale(1)}>
+                100%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(1.5)}>
+                150%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(2)}>
+                200%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(2.5)}>
+                250%
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
       <div className="flex-1 w-full max-h-screen">
-        <div ref={ref}>
-          <Document
-            file={url}
-            className="max-h-full"
-            loading={<LoadingView />}
-            onLoadError={() => {
-              toast({
-                title: "Error loading PDF",
-                description: "Please try again later",
-                variant: "destructive",
-              });
-            }}
-            onLoadSuccess={({ numPages }) => {
-              setNumberOfPages(numPages);
-            }}
-          >
-            <Page
+        <SimpleBar autoHide={false} className="max-h-[calc(100vh - 10rem)]">
+          <div ref={ref}>
+            <Document
+              file={url}
+              className="max-h-full"
               loading={<LoadingView />}
-              width={width ? width : 1}
-              pageNumber={currentPage}
-            />
-          </Document>
-        </div>
+              onLoadError={() => {
+                toast({
+                  title: "Error loading PDF",
+                  description: "Please try again later",
+                  variant: "destructive",
+                });
+              }}
+              onLoadSuccess={({ numPages }) => {
+                setNumberOfPages(numPages);
+              }}
+            >
+              <Page
+                loading={<LoadingView />}
+                width={width ? width : 1}
+                pageNumber={currentPage}
+                scale={scale}
+              />
+            </Document>
+          </div>
+        </SimpleBar>
       </div>
     </div>
   );
