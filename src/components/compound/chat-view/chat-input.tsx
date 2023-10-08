b3da0830890270ecbe1fe04074e7ee4a48c9092a
conrad mugabe <conradmugabe@gmail.com>
2023-10-08 15:59:49 +0300

    feat: fleshed out chat input component

--- src/components/compound/chat-view/chat-input.tsx
+++ src/components/compound/chat-view/chat-input.tsx
@@ -1,7 +1,35 @@
+import { Button } from "@/components/common/button";
+import { Textarea } from "@/components/common/text-area";
+import { SendIcon } from "lucide-react";
+
 type Props = {
   isDisabled?: boolean;
 };
 
 export function ChatInput({ isDisabled }: Props) {
-  return null;
+  return (
+    <div className="absolute bottom-0 left-0 w-full">
+      <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <div className="relative">
+              <Textarea
+                placeholder="Enter your question..."
+                rows={1}
+                maxRows={4}
+                autoFocus
+                className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+              />
+              <Button
+                aria-label="send message"
+                className="absolute bottom-1.5 right-[8px]"
+              >
+                <SendIcon className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
